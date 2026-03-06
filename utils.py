4a921cc377447cc3531b245056def810cc3047e3
akovb982-jpg <akovb982@gmail.com>
2026-03-06 10:03:35 +0200

    Yakov

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * factorial(n - 1)
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
